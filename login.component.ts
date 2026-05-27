f1e107ad371c39a5e6e860ce647555eb63bdc707
suji07-web <rangarajansucharita@gmail.com>
2026-05-27 12:14:44 +0530

    Add files via upload

--- login.component.ts
+++ login.component.ts
@@ -7,9 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  errorMessage: string = '';
+  usernameError: boolean = false;
+  passwordError: boolean = false;
+
+
   constructor(private router: Router) {}
 
   login() {
-    this.router.navigate(['/home']);
+
+    this.usernameError = false;
+    this.passwordError = false;
+    this.errorMessage = '';
+
+    if (this.username.trim() !== 'admin') {
+      this.usernameError = true;
+    }
+
+    if (this.password.trim() !== '1234') {
+      this.passwordError = true;
+    }
+
+    if (!this.usernameError && !this.passwordError) {
+      this.router.navigate(['/home']);
+    }
+    else {
+      this.errorMessage = 'Invalid username or password';
+    }
   }
-}
+  }
+
+
